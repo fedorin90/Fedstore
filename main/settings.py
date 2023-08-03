582d814085df8b65b5b00aec7cedefd889028cce
fedorin90 <fedorin.mir@gmail.com>
2023-08-03 20:47:49 +0300

    some fix

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-duc14mos*04n%w2&j!oltayrs^y%3k#1tf90hgt@aqiv4dhz2w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fedorin.mir@gmail.com'
-EMAIL_HOST_PASSWORD = 'ijnpvfspfnwfbvpk'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
